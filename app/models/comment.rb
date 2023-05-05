c5935e84f51dfd8c6bdd1b0534f1a417ef3d6d8d
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-05 11:31:30 +0200

    update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :parent, polymorphic: true
   belongs_to :author, class_name: "User"
   belongs_to :gossip
-  has_many :likes
+  has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :sent_private_messages, foreign_key: "sender_id"
   has_many :recipients
-  has_many :private_messages, through: :recipients, source: :private_message
-  has_many :comments
-  has_many :likes
+  has_many :received_private_messages, through: :recipients, source: :private_message, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
