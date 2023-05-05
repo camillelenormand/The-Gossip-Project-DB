01cff06db8ba2ad138f6b8430fea3e848dffa6c6
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-05 11:27:05 +0200

    update models

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,6 @@
 class Gossip < ApplicationRecord
   belongs_to :user
   has_many :JoinTableGossipsTags
+  has_many :comments
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :sent_private_messages, foreign_key: "sender_id"
   has_many :recipients
-  has_many :received_private_messages, through: :recipients, source: :private_message
+  has_many :private_messages, through: :recipients, source: :private_message
+  has_many :comments
+  has_many :likes
 end
